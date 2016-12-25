9ef8e6c2d74ae058f6213a41cad515c767064f29
Nada Alaa <nadaalaa.na@gmail.com>
2016-12-25 18:50:12 +0200

    Testing

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.searchQueryTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.parallelRadioButton = new System.Windows.Forms.RadioButton();
+            this.parallelOmpRadioButton = new System.Windows.Forms.RadioButton();
             this.sequentialRadioButton = new System.Windows.Forms.RadioButton();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.parallelGPURadioButton = new System.Windows.Forms.RadioButton();
             this.resultsListBox = new System.Windows.Forms.ListBox();
             this.numOfThreadsNumericUpDown = new System.Windows.Forms.NumericUpDown();
             this.label2 = new System.Windows.Forms.Label();
@@ -49,109 +50,121 @@
             // searchQueryTextBox
             // 
             this.searchQueryTextBox.Font = new System.Drawing.Font("Tahoma", 12.5F);
-            this.searchQueryTextBox.Location = new System.Drawing.Point(263, 51);
+            this.searchQueryTextBox.Location = new System.Drawing.Point(526, 98);
+            this.searchQueryTextBox.Margin = new System.Windows.Forms.Padding(6);
             this.searchQueryTextBox.Multiline = true;
             this.searchQueryTextBox.Name = "searchQueryTextBox";
-            this.searchQueryTextBox.Size = new System.Drawing.Size(368, 143);
+            this.searchQueryTextBox.Size = new System.Drawing.Size(732, 271);
             this.searchQueryTextBox.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 12.5F);
-            this.label1.Location = new System.Drawing.Point(259, 27);
+            this.label1.Location = new System.Drawing.Point(518, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 21);
+            this.label1.Size = new System.Drawing.Size(221, 41);
             this.label1.TabIndex = 1;
             this.label1.Text = "Search Query";
             // 
-            // parallelRadioButton
+            // parallelOmpRadioButton
             // 
-            this.parallelRadioButton.AutoSize = true;
-            this.parallelRadioButton.Checked = true;
-            this.parallelRadioButton.Font = new System.Drawing.Font("Tahoma", 12.5F);
-            this.parallelRadioButton.Location = new System.Drawing.Point(17, 27);
-            this.parallelRadioButton.Name = "parallelRadioButton";
-            this.parallelRadioButton.Size = new System.Drawing.Size(137, 25);
-            this.parallelRadioButton.TabIndex = 3;
-            this.parallelRadioButton.TabStop = true;
-            this.parallelRadioButton.Text = "Parallel Search";
-            this.parallelRadioButton.UseVisualStyleBackColor = true;
+            this.parallelOmpRadioButton.AutoSize = true;
+            this.parallelOmpRadioButton.Checked = true;
+            this.parallelOmpRadioButton.Font = new System.Drawing.Font("Tahoma", 12.5F);
+            this.parallelOmpRadioButton.Location = new System.Drawing.Point(34, 55);
+            this.parallelOmpRadioButton.Margin = new System.Windows.Forms.Padding(6);
+            this.parallelOmpRadioButton.Name = "parallelOmpRadioButton";
+            this.parallelOmpRadioButton.Size = new System.Drawing.Size(158, 45);
+            this.parallelOmpRadioButton.TabIndex = 3;
+            this.parallelOmpRadioButton.TabStop = true;
+            this.parallelOmpRadioButton.Text = "Parallel";
+            this.parallelOmpRadioButton.UseVisualStyleBackColor = true;
+            this.parallelOmpRadioButton.CheckedChanged += new System.EventHandler(this.parallelRadioButton_CheckedChanged);
             // 
             // sequentialRadioButton
             // 
             this.sequentialRadioButton.AutoSize = true;
             this.sequentialRadioButton.Font = new System.Drawing.Font("Tahoma", 12.5F);
-            this.sequentialRadioButton.Location = new System.Drawing.Point(17, 58);
+            this.sequentialRadioButton.Location = new System.Drawing.Point(34, 112);
+            this.sequentialRadioButton.Margin = new System.Windows.Forms.Padding(6);
             this.sequentialRadioButton.Name = "sequentialRadioButton";
-            this.sequentialRadioButton.Size = new System.Drawing.Size(160, 25);
+            this.sequentialRadioButton.Size = new System.Drawing.Size(206, 45);
             this.sequentialRadioButton.TabIndex = 4;
-            this.sequentialRadioButton.Text = "Sequential Search";
+            this.sequentialRadioButton.Text = "Sequential";
             this.sequentialRadioButton.UseVisualStyleBackColor = true;
+            this.sequentialRadioButton.CheckedChanged += new System.EventHandler(this.sequentialRadioButton_CheckedChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Tahoma", 12.5F);
-            this.label3.Location = new System.Drawing.Point(12, 27);
+            this.label3.Location = new System.Drawing.Point(24, 52);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(152, 21);
+            this.label3.Size = new System.Drawing.Size(310, 41);
             this.label3.TabIndex = 5;
             this.label3.Text = "Number of Threads";
             // 
-            // button1
+            // searchButton
             // 
-            this.button1.Font = new System.Drawing.Font("Tahoma", 12.5F);
-            this.button1.Location = new System.Drawing.Point(16, 154);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(217, 40);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Search";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.searchButton.Font = new System.Drawing.Font("Tahoma", 12.5F);
+            this.searchButton.Location = new System.Drawing.Point(28, 391);
+            this.searchButton.Margin = new System.Windows.Forms.Padding(6);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(434, 77);
+            this.searchButton.TabIndex = 7;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.parallelRadioButton);
+            this.groupBox1.Controls.Add(this.parallelGPURadioButton);
+            this.groupBox1.Controls.Add(this.parallelOmpRadioButton);
             this.groupBox1.Controls.Add(this.sequentialRadioButton);
             this.groupBox1.Font = new System.Drawing.Font("Tahoma", 12.5F);
-            this.groupBox1.Location = new System.Drawing.Point(15, 61);
+            this.groupBox1.Location = new System.Drawing.Point(30, 117);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(6);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(217, 87);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(6);
+            this.groupBox1.Size = new System.Drawing.Size(434, 252);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Search Mode";
+            // 
+            // parallelGPURadioButton
+            // 
+            this.parallelGPURadioButton.AutoSize = true;
+            this.parallelGPURadioButton.Font = new System.Drawing.Font("Tahoma", 12.5F);
+            this.parallelGPURadioButton.Location = new System.Drawing.Point(34, 169);
+            this.parallelGPURadioButton.Margin = new System.Windows.Forms.Padding(6);
+            this.parallelGPURadioButton.Name = "parallelGPURadioButton";
+            this.parallelGPURadioButton.Size = new System.Drawing.Size(233, 45);
+            this.parallelGPURadioButton.TabIndex = 5;
+            this.parallelGPURadioButton.Text = "Parallel GPU";
+            this.parallelGPURadioButton.UseVisualStyleBackColor = true;
+            this.parallelGPURadioButton.CheckedChanged += new System.EventHandler(this.parallelGPURadioButton_CheckedChanged);
             // 
             // resultsListBox
             // 
             this.resultsListBox.Font = new System.Drawing.Font("Tahoma", 12.5F);
             this.resultsListBox.FormattingEnabled = true;
-            this.resultsListBox.ItemHeight = 21;
-            this.resultsListBox.Items.AddRange(new object[] {
-            "215",
-            "3435",
-            "4598",
-            "18422",
-            "36501",
-            "46548",
-            "656878",
-            "487855",
-            "495352",
-            "1577884",
-            "45664879",
-            "87987879",
-            "4545620022"});
-            this.resultsListBox.Location = new System.Drawing.Point(20, 69);
+            this.resultsListBox.ItemHeight = 40;
+            this.resultsListBox.Location = new System.Drawing.Point(31, 134);
+            this.resultsListBox.Margin = new System.Windows.Forms.Padding(6);
             this.resultsListBox.Name = "resultsListBox";
-            this.resultsListBox.Size = new System.Drawing.Size(128, 193);
+            this.resultsListBox.Size = new System.Drawing.Size(252, 364);
             this.resultsListBox.TabIndex = 9;
             // 
             // numOfThreadsNumericUpDown
             // 
             this.numOfThreadsNumericUpDown.Font = new System.Drawing.Font("Tahoma", 12.5F);
-            this.numOfThreadsNumericUpDown.Location = new System.Drawing.Point(170, 27);
+            this.numOfThreadsNumericUpDown.Location = new System.Drawing.Point(340, 52);
+            this.numOfThreadsNumericUpDown.Margin = new System.Windows.Forms.Padding(6);
             this.numOfThreadsNumericUpDown.Name = "numOfThreadsNumericUpDown";
-            this.numOfThreadsNumericUpDown.Size = new System.Drawing.Size(63, 28);
+            this.numOfThreadsNumericUpDown.Size = new System.Drawing.Size(126, 48);
             this.numOfThreadsNumericUpDown.TabIndex = 10;
             this.numOfThreadsNumericUpDown.Value = new decimal(new int[] {
             4,
@@ -163,9 +176,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Tahoma", 12.5F);
-            this.label2.Location = new System.Drawing.Point(12, 24);
+            this.label2.Location = new System.Drawing.Point(24, 46);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(150, 21);
+            this.label2.Size = new System.Drawing.Size(302, 41);
             this.label2.TabIndex = 11;
             this.label2.Text = "Search Completed!";
             // 
@@ -173,9 +187,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Tahoma", 12.5F);
-            this.label4.Location = new System.Drawing.Point(12, 45);
+            this.label4.Location = new System.Drawing.Point(24, 87);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(362, 21);
+            this.label4.Size = new System.Drawing.Size(732, 41);
             this.label4.TabIndex = 12;
             this.label4.Text = "14 results were found at the following locations";
             // 
@@ -186,9 +201,11 @@
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Font = new System.Drawing.Font("Tahoma", 12.5F);
-            this.groupBox2.Location = new System.Drawing.Point(16, 216);
+            this.groupBox2.Location = new System.Drawing.Point(28, 480);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(6);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(615, 283);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(6);
+            this.groupBox2.Size = new System.Drawing.Size(1230, 544);
             this.groupBox2.TabIndex = 13;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Search Results";
@@ -197,24 +214,26 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Tahoma", 12.5F);
-            this.label5.Location = new System.Drawing.Point(168, 24);
+            this.label5.Location = new System.Drawing.Point(322, 46);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(298, 21);
+            this.label5.Size = new System.Drawing.Size(604, 41);
             this.label5.TabIndex = 13;
             this.label5.Text = "Search Duration was 1.03254 seconds.";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(643, 510);
+            this.ClientSize = new System.Drawing.Size(1286, 1039);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.numOfThreadsNumericUpDown);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.searchButton);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.searchQueryTextBox);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
@@ -231,10 +250,10 @@
 
         private System.Windows.Forms.TextBox searchQueryTextBox;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RadioButton parallelRadioButton;
+        private System.Windows.Forms.RadioButton parallelOmpRadioButton;
         private System.Windows.Forms.RadioButton sequentialRadioButton;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button searchButton;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.ListBox resultsListBox;
         private System.Windows.Forms.NumericUpDown numOfThreadsNumericUpDown;
@@ -242,6 +261,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton parallelGPURadioButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,39 +14,60 @@ namespace HPCProject
     public partial class Form1 : Form
     {
         public string searchQuery;
-        public int searchMode;
-        public int threadsNumber;
-        public List<int> results = new List<int>();
+        public mode searchMode;
+        public int num_threads;
+        int length;
+        int[] positions;
+
         public Form1()
         {
             InitializeComponent();
-            //////////////testing 
-            int x = Wrap.subtract(10, 5);
-            string tmp = "islam omar islam islam";
-            IntPtr textsearch = Wrap.Create_TextSearch_Obj(tmp); // 
-            int length;
-            int[] positions;
-            Wrap.Execute_SearchforWord(textsearch, "islam", mode.Serial, out positions, out length);
-            double time = Wrap.Execute_GetElapsedTime(textsearch);
-            Wrap.Delete_TextSearch_Obj(textsearch); //don't forget
-            int vx = 0;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void searchButton_Click(object sender, EventArgs e)
         {
+            if (!IsDataComplete())
+            {
+                MessageBox.Show("Please Enter Search Query.");
+                return;
+            }
+
+
             searchQuery = this.searchQueryTextBox.Text;
-            threadsNumber = Convert.ToInt32(this.numOfThreadsNumericUpDown.Value);
-            if(this.parallelRadioButton.Checked==true)
-            {
-                this.searchMode = 0;
-            }
-            else
-            {
-                this.searchMode = 1;
-            }
+            num_threads = Convert.ToInt32(this.numOfThreadsNumericUpDown.Value);
+            
+            IntPtr textsearch = Wrap.Create_TextSearch_Obj(searchQuery);
+            Wrap.Execute_SearchforWord(textsearch, searchQuery, searchMode, out positions, out length);
+            double time = Wrap.Execute_GetElapsedTime(textsearch);
+            Wrap.Delete_TextSearch_Obj(textsearch); //don't forget
+        }
+
+        private void DisplayResults()
+        {
+            resultsListBox.Items.Add(positions);
+        }
+
+        private bool IsDataComplete()
+        {
+            return searchQueryTextBox.Text.Length != 0;
+        }
+
+        private void parallelRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            searchMode = mode.Parallel;
+        }
+
+        private void sequentialRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            searchMode = mode.Serial;
+        }
+
+        private void parallelGPURadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            searchMode = mode.ParallelGPU;
         }
     }
-    public enum mode { Parallel, Serial };
+    public enum mode { Parallel, Serial, ParallelGPU };
     public class Wrap
     {
         [DllImport(@"D:\Text-Search-master\Release\TextSearchCpluse.dll", CallingConvention = CallingConvention.Cdecl)]
@@ -59,7 +80,5 @@ namespace HPCProject
         public static extern double Execute_GetElapsedTime(IntPtr Obj);
         [DllImport(@"D:\Text-Search-master\Release\TextSearchCpluse.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern void Execute_SearchforWord(IntPtr Obj, string keyword, mode searchMode, [MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 4)] out int[] data, out int count);
-
     }
-
 }
